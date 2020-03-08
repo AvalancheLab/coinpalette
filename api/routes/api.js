cc08e1ba8318344933f2c8130122d1e2c87f9ce6
AvalancheLab <avalanchelab@protonmail.com>
2020-03-08 00:44:15 +0000

    Update route paths.

--- api/routes/api.js
+++ api/routes/api.js
@@ -3,12 +3,12 @@ const app = express();
 const getColors  = require('../libs/getColors');
 const getCoinList = require('../libs/getCoinList');
 
-app.get('/api/list', ( _, res ) => {
+app.get('/list', ( _, res ) => {
     const coinList = getCoinList();
     res.json( coinList );
 });
 
-app.get('/api/colors', ( req, res ) => {
+app.get('/colors', ( req, res ) => {
     const args = req.query.coins;
     const coins = args ? args.split(',') : [];
     const colors = getColors({ "coins": coins });
