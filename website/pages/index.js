caee43e92f76c4345b5e7fd3e52deb70f4f83a7f
AvalancheLab <avalanchelab@protonmail.com>
2020-05-16 05:25:16 +0100

    Add global styling and component imports.

--- website/pages/index.js
+++ website/pages/index.js
@@ -1,15 +1,32 @@
 import Head from 'next/head'
-
+import styled from 'styled-components'
+import Footer from '../sections/Footer'
+import Header from '../components/Header'
+import Docs from '../sections/Docs';
 
 export default Home = () => {
+
+  const GlobalStyle = styled.globalStyle`
+    body {
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }  
+  `
+
+  const Footer = styled(Footer)`
+    margin-top: auto;
+  `
+
   return (
     <>
-
-      <style jsx>{`
-        
-      `}</style>
-
+      <Head>
+        <title>CoinPalette</title>
+      </Head>
+      <GlobalStyle/>
+      <Header/>
+      <Docs/>
+      <Footer/>
     </>
-
   )
 }
