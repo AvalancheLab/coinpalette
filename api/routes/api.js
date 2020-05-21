f38d1b8c0e2128e3bc6dc2bc7f081f7d1216b639
AvalancheLab <avalanchelab@protonmail.com>
2020-05-21 10:59:15 +0100

    Change to router object.

--- api/routes/api.js
+++ api/routes/api.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const server = express();
+const router = express.Router();
 const getColors  = require('../libs/getColors');
 const getCoinList = require('../libs/getCoinList');
 
-server.get('/list', ( _, res ) => {
+router.get('/list', ( _, res ) => {
     const coinList = getCoinList();
     res.json( coinList );
 });
 
-server.get('/colors', ( req, res ) => {
+router.get('/colors', ( req, res ) => {
     const args = req.query.coins;
     const coins = args ? args.split(',') : [];
     const colors = getColors({ "coins": coins });
     res.json( colors );
 });
 
-module.exports = server;
+module.exports = router;
