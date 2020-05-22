906bc582c207258fb613c37ec1f28d5d75d5aeb1
AvalancheLab <avalanchelab@protonmail.com>
2020-05-22 22:52:31 +0100

    Add palette generation function.

--- api/libs/generatePalettes.js
+++ api/libs/generatePalettes.js
@@ -10,32 +10,30 @@ async function generatePalettes () {
     // Fetch all coins from CoinGecko API.
     let coins = [];
     let i = 1
-    
     while (true) {
         console.log(`Fetching coins for page ${i}.`)
         const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${i}`;
         let result = await jsonFetch(url).catch(_ => [])
         // console.log(result)
-        if (result.length === 0) break;
+        if (result.length === 0) break
         coins.push(...result)
         i++
     }
 
-    // coins = await jsonFetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${1}`)
-    // .catch(_ => []);
-
     if (coins.length === 0) return
 
     // Create temporary download folder.
-    const downloadFolder = path.join(__dirname, './tmp/')
+    const downloadFolder = path.join(__dirname, './images/')
     if (!existsSync(downloadFolder)){
         mkdirSync(downloadFolder)
     }
 
     // Download images.
-    coins = await Promise.all(coins.map(async (coin, idx) => {
+    coins = await Promise.all(coins.map(async coin => {
         const localImagePath = path.join(downloadFolder, `${coin.id}.png`)
-        await downloadFile(coin.image, localImagePath).catch(err => new Error(err))
+        if (!existsSync(localImagePath)) {
+            await downloadFile(coin.image, localImagePath).catch(err => new Error(err))
+        }
         coin.localImagePath = localImagePath
         return coin;
     }))
@@ -43,17 +41,21 @@ async function generatePalettes () {
 
     // Generate palettes from downloaded images.
     let palettes = {};
+    coins.forEach(({ id }) => {
+        palettes[id] = {}
+    });
+
     await Promise.all(coins.map(async ({ id, localImagePath }) => {
         
         // Generate color palette from image.
         let colors = await vibrant.from(localImagePath).getPalette()
+        .catch(err => new Error(err))
 
         // Create palette object containing rgb, hex and hsl fields.
-        Object.keys(colors).map(variant => {
+        Object.keys(colors).forEach(variant => {
             const color = colors[variant];
             if (!color) return console.log(`Failed to get palettes for ${id}`)
-            if (!palettes[id]) palettes[id] = {}
-            palettes[id][variant] = {
+            palettes[id][variant.toLowerCase()] = {
                 rgb : color.getRgb(),
                 hex : color.getHex(),
                 hsl : color.getHsl()
@@ -65,7 +67,6 @@ async function generatePalettes () {
     writeFileSync(path.join(__dirname, "../colorList.json"), JSON.stringify(palettes))
 
     // Delete temporary download folder.
-    removeSync(downloadFolder)
     console.log('Finished.')
 }
 
