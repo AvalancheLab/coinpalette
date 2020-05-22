4904feed0f7d48f7952532203ecc64dc41659187
AvalancheLab <avalanchelab@protonmail.com>
2020-05-22 20:03:20 +0100

    Add static file delivery.

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const server = express();
-const path = require('path');
-const API_Router = require('./api/routes/api');
+const express = require('express')
+const server = express()
+const path = require('path')
+const API_Router = require('./api/API_Router')
+const PORT = 3000
 
+// Action to be performed for all requests.
+server.use((req, res, next) => {
+    // action
+    next()
+})
+
+// Serve static site files.
 server.use(express.static(path.join(__dirname, './build')))
-server.use(API_Router);
+
+// Process API requests.
+server.use(API_Router)
+
+// Handle any requests that don't match the ones above.
+server.get('*', ( _, res ) => res.sendStatus(404))
+
+server.listen(PORT)
+console.log(`Server started on port ${PORT}.`)
 
 
-// Handles any requests that don't match the ones above.
-server.get('*', ( _, res ) => res.sendStatus(404));
 
-server.listen(3000);
 
 
