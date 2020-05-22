10e8f8d9c1f36377cc20ed418d0272596c864875
AvalancheLab <avalanchelab@protonmail.com>
2020-05-22 22:53:05 +0100

    Remove format selection, return all format.

--- api/libs/getColors.js
+++ api/libs/getColors.js
@@ -1,5 +1,4 @@
 const colorList = require('../colorList.json');
-const convert = require('color-convert');
 
 // Get a list of colors for specified colors and color format.
 module.exports = function getColors({ coins, format }) {
@@ -8,19 +7,5 @@ module.exports = function getColors({ coins, format }) {
     if(!coins) return colorList 
 
     // Filter based on specified coins.
-    let matching = coins.map(coin => ({[coin] : colorList[coin]}));
-    
-    // Return RGB is format is not specified.
-    if (!format) return matching
-
-    // Convert to specified format if HEX or HSL are chosen.
-    if (format.toLowerCase() === 'hex' || 'hsl') {
-        matching = matching.map(coin => {
-            return Object.keys(coin).map(color => {
-                return convert.rgb[format](...color)
-            })
-        })
-    }
-    
-    return matching;
+    return coins.map(coin => ({[coin] : colorList[coin]}));
 }
